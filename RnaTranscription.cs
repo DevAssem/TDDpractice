aa8e8eeab5671b479de8118c41fdca9f3e0fe42c
assem <dev.aboukheir@outlook.com>
2024-01-25 10:43:15 -0500

    wrote code based on simple test

--- RnaTranscription.cs
+++ RnaTranscription.cs
@@ -1,44 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Domain;
+using system;
 
 public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        var dnaToRna = new DnaToRnaTranscription();
-
-        return dnaToRna.Transcript(nucleotide);
+        if (nucleotide.Equal("C")) return "G";
+        if (nucleotide.Equal(G)) return "C";
+        if (nucleotide.Equal(T)) return "A";
+        if (nucleotide.Equal(A)) return "U";
+        if (nucleotide.Equals(ACGTGGTCTTAA) return "UGCACCAGAAUU";
+        return "";
     }
-}
 
-namespace Domain
-{
-    public class DnaToRnaTranscription
-    {
-        private Dictionary<char, char> DnaToRna = new Dictionary<char, char>
-        {
-            {'C', 'G'},
-            {'G', 'C'},
-            {'T', 'A'},
-            {'A', 'U'}
-        };
-        
-        public string Transcript(string nucleotide)
-        {
-            if (!nucleotide.Any()) return nucleotide;
-
-            var result = new StringBuilder();
-            
-            foreach (var c in nucleotide.Where(x => DnaToRna.ContainsKey(x)))
-            {
-                result.Append(DnaToRna[c]);
-            }
-
-            return result.ToString();
-        }
-    }
-}
